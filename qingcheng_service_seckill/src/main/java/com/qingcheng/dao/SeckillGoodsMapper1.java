a26cc0f8c8ebb2076a4d82bc6612e1134b0491cc
mrdeng123it <936959479@qq.com>
2019-10-20 23:07:18 +0800

    测试Excel导出功能2

--- qingcheng_service_seckill/src/main/java/com/qingcheng/dao/SeckillGoodsMapper1.java
+++ qingcheng_service_seckill/src/main/java/com/qingcheng/dao/SeckillGoodsMapper1.java
@@ -3,5 +3,8 @@ package com.qingcheng.dao;
 import com.qingcheng.pojo.seckill.SeckillGoods;
 import tk.mybatis.mapper.common.Mapper;
 
+/**
+ *
+ */
 public interface   SeckillGoodsMapper1 extends Mapper<SeckillGoods> {
 }
